dcc8d7eccfbe378f5a9756447f4732a26915fa6f
SFBB <anzhe990206@outlook.com>
2023-05-26 21:10:26 +0800

    fix comment typo

--- web_scraper/src/main.rs
+++ web_scraper/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let movie_number = 100;
-    // println is a macro, we need mark ! as a 
+    // println is a macro, we need mark ! as a suffix.
     println!("We are scraping top {} movies from imdb!", movie_number);
     let response = reqwest::blocking::get(
         "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100",

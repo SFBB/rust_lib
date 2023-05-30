6d43119ad84c87c192333fbfbaf4be564af94d3d
SFBB <anzhe990206@outlook.com>
2023-05-30 22:40:06 +0800

    add how to use async and concurrent programming in rust.

---
+++ concurrent/src/main.rs
@@ -0,0 +1,11 @@
+
+async fn Hello_World_Async() {
+    std::thread::sleep(std::time::Duration::from_secs(6));
+    println!("Hello, world! From Async!\n");
+}
+
+#[tokio::main]
+async fn main() {
+    tokio::spawn(Hello_World_Async());
+    println!("Hello, world! From main!\n");
+}
